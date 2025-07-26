890b231047467dca402be712582123c17e82c878
Zain Ali <zainaliworkmail3830@gmail.com>
2025-07-26 14:33:59 -0700

    code fixes

--- Lecture4/code/src/routes/users.js
+++ Lecture4/code/src/routes/users.js
@@ -5,5 +5,5 @@ import validateUser from '../middlewares/validateUser.js';
 
 const router=express.Router();
 router.get('/getusers',getUsers);
-router.post('/createuser',createUser,validateUser);
+router.post('/createuser',validateUser,createUser);
 export default router;
--- Lecture5/code/src/routes/userroute.js
+++ Lecture5/code/src/routes/userroute.js
@@ -3,5 +3,5 @@ const router = express.Router();
 import { registerUser } from '../controllers/usercontroller.js';
 import validateUser from '../middleware/validateuser.js';
 
-router.post('/register', registerUser,validateUser);
+router.post('/register',validateUser, registerUser);
 export default router;
